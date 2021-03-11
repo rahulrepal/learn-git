3d490d84c36ad27cb9b653c79a00774ed63dca5a
rahulrepal <rrepal8@gmail.com>
2021-03-11 19:30:39 +0530

    Fixed #1 --Added name validation

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function sayMyName(name){
+    
+    if(name.search(/[^A-Za-z\s]/) != -1)
+        throw "Invalid name"
+    
     return name
 }
 
-console.log(sayMyName(name="timothy"))
+console.log(sayMyName(name="ts1imothy"))

ec87d6203b7ac88cc0fba1265bbbd342418cb394
rahulrepal <rrepal8@gmail.com>
2021-03-24 12:13:20 +0530

    Added etl script

---
+++ init.js
@@ -0,0 +1,14 @@
+//import db
+
+analysis_data = null
+
+function get_analysis_data(){
+
+    analysis_data='89234'
+    return analysis_data
+}
+
+function update(){
+
+    analysis_data = '89234'
+}

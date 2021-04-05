63570b5e68231ac7ddafa2b95a3530bc667e72f5
rahulrepal <rrepal8@gmail.com>
2021-04-05 15:17:15 +0530

    Added code to take database name from command-line and change database accordingly

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function sayMyName(name){
 console.log(sayMyName(name="rad"))
 
 //Adding some new features here
+supported_databases = ["mssql"]
+function switchDatabase(database_name){
+    if(!supported_databases.includes(database_name))
+    {
+        console.log("the database is not support")
+    }
+    else if(database_name="mssql"){
+        console.log("print mssql")
+    }
+    else{
+        console.log("Please provide appropriate database name")
+    }
+    
+}
+
+switchDatabase("mssq1l")

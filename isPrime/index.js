e0a4479121bd7687a714eea9c503a0426bfde4dc
rahulrepal <rrepal8@gmail.com>
2021-07-26 09:35:12 +0530

    Renamed number variable to input_number

--- isPrime/index.js
+++ isPrime/index.js
@@ -1,29 +1,29 @@
 // program to check if a number is prime or not
 
 // take input from the user
-const number = parseInt(prompt("Enter a positive number: "));
+const input_number = parseInt(prompt("Enter a positive number: "));
 let isPrime = true;
 
 // check if number is equal to 1
-if (number === 1) {
+if (input_number === 1) {
     console.log("1 is neither prime nor composite number.");
 }
 
 // check if number is greater than 1
-else if (number > 1) {
+else if (input_number > 1) {
 
     // looping through 2 to number-1
-    for (let i = 2; i < number; i++) {
-        if (number % i == 0) {
+    for (let i = 2; i < input_number; i++) {
+        if (input_number % i == 0) {
             isPrime = false;
             break;
         }
     }
 
     if (isPrime) {
-        console.log(`${number} is a prime number`);
+        console.log(`${input_number} is a prime number`);
     } else {
-        console.log(`${number} is a not prime number`);
+        console.log(`${input_number} is a not prime number`);
     }
 }
 
